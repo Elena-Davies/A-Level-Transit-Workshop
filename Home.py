06c33535948638d409793616abfef5238f85d707
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-09 23:37:08 +0000

    add text

--- Home.py
+++ Home.py
@@ -16,5 +16,10 @@ st.title('A-Level Transit Method: The Transit Trail!')
 
 # adding pages
 st.write("# Welcome to The Transit Trail!")
+st.write('You have been tasked with finding habitable exoplanets to find potential signs of life on other planets!')
+st.write('However, before you can find E.T., you have to learn what to look for and how!')
+st.write('This is where this workshop comes in! You will complete missions and levels to become a planet hunter in no time!')
+st.write('On your journey, you will encounter levels, missions, different terrains and chances of rain! These will announce which levels are more harder than others, so take care out in the rainy and rocky terrains!')
+st.write('Are you ready to start your journey? If so select a level from the sidebar to start!')
 st.sidebar.success("Select a level above.")
     
