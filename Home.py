a775a1d329334e8e72aa6adf58dee2a5cad9f7c2
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-10 23:48:09 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,50 +16,9 @@ st.title('A-Level Transit Method: The Transit Trail!')
 
 st.write("# Welcome to The Transit Trail!")
 
-# Parameters
-star_radius = 1.0
-planet_radius = 0.3
-orbit_radius = 3.0
-angular_velocity = 0.02
-frames = 315
-
-with _lock:
-    # Create figure and axis
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.set_aspect('equal')
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_title('Planet Orbit')
-
-    star = plt.Circle((0, 0), star_radius, color='yellow')
-    planet = plt.Circle((0, orbit_radius), planet_radius, color='blue')
-
-    ax.add_patch(star)
-    planet_patch = ax.add_patch(planet)
-
-    def update(frame):
-            angle = frame * angular_velocity
-            # Update position of the planet
-            x_planet = orbit_radius * np.cos(angle)
-            y_planet = orbit_radius * np.sin(angle)
-            planet_patch.set_center((x_planet, y_planet))
-            # Check if the planet is behind the star in both views
-            if np.pi/2 < angle % (2 * np.pi) < 3 * np.pi / 2:
-                planet_patch.set_alpha(0.2)
-            else:
-                planet_patch.set_alpha(1.0)
-            return planet_patch
-    
-    # Create animation
-    ani = FuncAnimation(fig, update, frames=frames, blit=True)
-    # Convert animation to HTML
-    #Convert the animation to HTML format using to_jshtml() method of the FuncAnimation object.
-    #html = ani.to_html5_video()
-    # Display the animation in Streamlit
-    components.html(ani.to_jshtml(), height=100, width=300)
-
+video_file = open('planet_orbit_animation.mp4, ')
+video_bytes = video_file.read()
+st.video(video_bytes)
 
 st.write('You have been tasked with finding habitable exoplanets to find potential signs of life on other planets!')
 st.write('However, before you can find E.T., you have to learn what to look for and how!')
