7d2d567974bbfaf5814e48f96bbc162979f713be
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-19 16:12:47 +0000

    remove lock

--- pages/3_Level_Three.py
+++ pages/3_Level_Three.py
@@ -8,7 +8,7 @@ import time
 # use the non-interactive Agg backend to be more thread safe
 #mpl.use("agg")
 from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
+#_lock = RendererAgg.lock
 from matplotlib.animation import FuncAnimation
 
 st.markdown('# Level Three: Atmospheres and Spectra')
