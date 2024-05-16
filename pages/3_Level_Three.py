2397b452017ef1d58597cbfe1db1ac061a9af39a
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-05-16 18:19:13 +0100

    remove unused imports

--- pages/3_Level_Three.py
+++ pages/3_Level_Three.py
@@ -1,15 +1,10 @@
 #Imports
 import streamlit as st
-import streamlit.components.v1 as components
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import time
 # use the non-interactive Agg backend to be more thread safe
 #mpl.use("agg")
-from matplotlib.backends.backend_agg import RendererAgg
 #_lock = RendererAgg.lock
-from matplotlib.animation import FuncAnimation
 
 st.markdown('# Level Three: Atmospheres and Spectra')
 st.sidebar.header("Level Three")
